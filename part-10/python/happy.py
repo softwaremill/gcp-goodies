d3b82eafae691feacfb93365792de663d06bb87f
softberries <krzysztof.grajek@googlemail.com>
2019-10-29 14:11:15 +0100

    part-10 added scripts for blog post - part 10

--- part-10/python/happy.py
+++ part-10/python/happy.py
@@ -7,24 +7,16 @@ import csv
 
 bucket_name = "softwaremill"
 csv_file = "softwaremill.csv"
-storage_client = storage.Client() #.from_service_account_json('/home/kris/Downloads/happy.json')
+storage_client = storage.Client()
 
 def getFiles(bucket_name):
     blobs = storage_client.list_blobs(bucket_name)
     return blobs
-    #     date_str = blob.name[:10]
-    #     date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
-    #     print(date_obj)
+
 
 
 def detect_face(bucket_name, image_name):
     print("Detecting faces for: " + image_name)
-    """Uses the Vision API to detect faces in the given file.
-    Args:
-        face_file: A file-like object containing an image with faces.
-    Returns:
-        An array of Face objects with information about the picture.
-    """
     joy_likelihoods = []
     client = vision.ImageAnnotatorClient()
     response = client.annotate_image({'image': {'source': {'image_uri': 'gs://'+bucket_name+'/'+image_name}},'features': [{'max_results': 50, 'type': vision.enums.Feature.Type.FACE_DETECTION}]})
@@ -36,7 +28,6 @@ def detect_face(bucket_name, image_name):
 def getLikelihoodsOfJoy():
     d = {}
     blobs = getFiles(bucket_name)
-    # blobs = list(blobsx)[:3]
     for blob in blobs:
         name = blob.name
         date_str = str(name[:10])
@@ -64,9 +55,7 @@ def getCSVLines(likelihoods):
             count = values.count(level)
             rows.append([key, level, count])
     return rows
-# print(getLikelihoodsOfJoy())
-# blobs = getFiles(bucket_name)
-# createCSV(csv_file)
+
 likelihoods = getLikelihoodsOfJoy()
 rows = getCSVLines(likelihoods)
 createCSV(csv_file, rows)
--- part-10/python/screenshot.py
+++ part-10/python/screenshot.py
@@ -1,34 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-from selenium import webdriver
 from pynput.keyboard import Key, Controller
 from selenium.webdriver.common import keys
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from google.cloud import storage
+from datetime import datetime
 from PIL import Image
 import os
 
-# websiteAddress = "http://www.foxcrow.com"
-websiteAddress = "http://www.softwaremill.com"
-profile = webdriver.FirefoxProfile()
-profile.set_preference('plugin.state.flash', 2)
-# profile.set_preference('general.useragent.override', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0')
-profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so','true')
-browser = webdriver.Firefox(profile)
-browser.implicitly_wait(30)
-browser.get(websiteAddress)
-# run_flush = browser.find_element_by_id('app')
-# run_flush.click()
-time.sleep(5)
-# keyboard = Controller()
-# keyboard.press(Key.alt)
-# keyboard.press('a')
-# keyboard.release('a')
-# keyboard.release(Key.alt)
-time.sleep(5)
-browser.save_screenshot('screen_shot2.png')
-
+def upload_to_bucket(blob_name, path_to_file, bucket_name):
+    storage_client = storage.Client.from_service_account_json('happy.json')
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(path_to_file)
+    return blob.public_url
 
 def resize(img_dir, img_name, ratio = 2):
     img_path = os.path.join(img_dir, img_name)
@@ -38,5 +28,62 @@ def resize(img_dir, img_name, ratio = 2):
         to_img_path = os.path.join(img_dir, 'big-'+img_name)
         img.save(to_img_path)
 
-resize('.','screen_shot2.png', 2)
+options = Options()
+url = "big-blue-button.url.com"
+websiteAddress = "http://"+url
+
+prefs = {
+    "profile.default_content_setting_values.plugins": 1,
+    "profile.content_settings.plugin_whitelist.adobe-flash-player": 1,
+    "profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player": 1,
+    "PluginsAllowedForUrls": websiteAddress
+}
+
+
+
+options.add_experimental_option("prefs",prefs)
+browser = webdriver.Chrome(options=options)
+
+
+browser.get("chrome://settings/content/siteDetails?site=http://"+url)
+time.sleep(2)
+actions = ActionChains(browser)
+
+actions = actions.send_keys(Keys.TAB * 21)
+actions = actions.send_keys(Keys.SPACE)
+actions = actions.send_keys("a")
+actions = actions.send_keys(Keys.ENTER)
+
+actions.perform()
+time.sleep(2)
+browser.implicitly_wait(30)
+browser.get(websiteAddress)
+time.sleep(2)
+username = browser.find_element_by_id('username')
+# do the same for password if needed
+username.click()
+username.send_keys("Kris", Keys.ENTER)
+time.sleep(1)
+
+
+keyboard = Controller()
+keyboard.press(Key.alt)
+keyboard.press('a')
+keyboard.release('a')
+keyboard.release(Key.alt)
+
+time.sleep(15)
+body = browser.find_element_by_id('content')
+body.click()
+actions = ActionChains(browser)
+actions = actions.send_keys(Keys.TAB * 5)
+actions = actions.send_keys(Keys.ENTER)
+
+actions.perform()
+time.sleep(5)
+browser.save_screenshot('screen_shot.png')
+dt = datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+
+resize('.','screen_shot.png', 2)
+upload_to_bucket(dt+"-screenshot.png","big-screen_shot.png", "softwaremill")
 browser.close()
